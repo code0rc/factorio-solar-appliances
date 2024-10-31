956144a01229948efc63615a6ebf197fc9a5ff0d
Armin Matthes <al@elio-systems.com>
2024-10-31 23:14:37 +0100

    Fix wrong return item for solar radar entity

--- prototypes/entity/solar-radar.lua
+++ prototypes/entity/solar-radar.lua
@@ -6,7 +6,7 @@ prototype.localised_name = { "entity-name.solar-appliances-solar-radar" }
 if prototype.minable then
     prototype.minable.result = nil
     prototype.minable.results = {
-        { type = "item", name = "solar-appliances-solar-lamp-item", amount = 1 }
+        { type = "item", name = "solar-appliances-solar-radar-item", amount = 1 }
     }
 end
 
